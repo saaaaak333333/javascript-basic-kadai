d935665427eea07c3afc744c878fab62050f484c
saaaaak333333 <saaaki.0055@gmail.com>
2024-12-21 16:22:21 +0900

    bmiの変数を修正

--- bmi.js
+++ bmi.js
@@ -1,7 +1,7 @@
 //変数の宣言・値の代入
 let weight = 68;
 let hight = 1.7;
-let bmi = 23.529411764705884;
+let bmi = weight / (hight * hight);
 
 // コンソールへの出力
-console.log(weight / (hight * hight));
+console.log(bmi);
